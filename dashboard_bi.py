fbf40f95bfbe513c95ea736642b515d759dd1ab9
Derciel <dercieljr2@gmail.com>
2025-06-18 14:25:20 -0300

    atualização autenticação 3.8

--- dashboard_bi.py
+++ dashboard_bi.py
@@ -53,7 +53,7 @@ authenticator = stauth.Authenticate(
     config["cookie"]["expiry_days"]
 )
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login(location="sidebar")
 
 if authentication_status is False:
     st.error("Usuário ou senha incorretos.")
